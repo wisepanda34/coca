74a8c2c6ae3c4fbfb1e962544f79d7f1e089f52b
Yevgen <evghen.naydenko@gmail.com>
2024-09-29 14:35:05 +0300

    deleted .output

--- .output/public/_nuxt/BbeOORRa.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as t,Z as o,o as c}from"./D54PhuMQ.js";const r={},a={class:"site"};function n(e,_){return c(),t("div",a,[o(e.$slots,"default")])}const d=s(r,[["render",n]]);export{d as default};
