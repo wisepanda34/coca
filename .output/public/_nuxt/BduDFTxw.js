2a249257062c5a950c2d45c36bfae4f02d74c4e9
Yevgen <evghen.naydenko@gmail.com>
2024-09-30 16:22:35 +0300

    Deploy 3 to GitHub Pages

---
+++ .output/public/_nuxt/BduDFTxw.js
@@ -0,0 +1,1 @@
+import{_ as s,c as t,a3 as o,o as c}from"./B76zslw0.js";const a={},r={class:"site"};function n(e,_){return c(),t("div",r,[o(e.$slots,"default")])}const d=s(a,[["render",n]]);export{d as default};
